d7f681a7751e9530104458f52953d09e3e2d6d74
Dandi Juliandi <dandi.juliandi@nawadata.id>
2024-09-21 09:18:51 +0700

    add swagger feature

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common/pipes';
+
+import { AppModule } from './app.module';
+
+const config = new DocumentBuilder()
+    .setTitle('Nest Training API')
+    .setDescription('This app is for my NestJS practice.')
+    .setVersion('1.0')
+    .build();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBase64, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     label: string;
 
+    @ApiProperty()
     @IsBase64()
     image: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsNumber()
     price: number;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     type: string;
